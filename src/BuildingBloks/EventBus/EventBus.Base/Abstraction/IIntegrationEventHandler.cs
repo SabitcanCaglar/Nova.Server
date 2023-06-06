fefebd27f2d6352410c89026590a2f0892222a6d
sabitcan.caglar <sabitcan.caglar@outlook.com>
2023-06-05 20:40:05 -0700

    feat(eventbus): add event bus manager.

---
+++ src/BuildingBloks/EventBus/EventBus.Base/Abstraction/IIntegrationEventHandler.cs
@@ -0,0 +1,13 @@
+using EventBus.Base.Events;
+
+namespace EventBus.Base.Abstraction;
+
+public interface IIntegrationEventHandler<TIntegrationEvent> : IIntegrationEventHandler where TIntegrationEvent : IntegrationEvent
+{
+    Task Handle(TIntegrationEvent @event);
+}
+
+public interface IIntegrationEventHandler
+{
+    
+}
